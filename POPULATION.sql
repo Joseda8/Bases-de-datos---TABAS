1cca5c0fa6717425d1ca26985bf209d3740f9355
Joseda8 <jd.montoya.s8@gmail.com>
2019-05-18 21:27:57 -0600

    Script de poblamiento

---
+++ POPULATION.sql
@@ -0,0 +1,9 @@
+INSERT INTO ROLE (role) VALUES('Flight dispatcher');
+INSERT INTO ROLE (role) VALUES('Administrative ');
+INSERT INTO ROLE (role) VALUES('Scanner');
+INSERT INTO ROLE (role) VALUES('Counter Agent');
+INSERT INTO ROLE (role) VALUES('Drug control');
+INSERT INTO ROLE (role) VALUES('Security');
+INSERT INTO ROLE (role) VALUES('Control tower');
+INSERT INTO ROLE (role) VALUES('Flight Attendant');
+INSERT INTO ROLE (role) VALUES('Packager');
