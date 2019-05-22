3c941b1d90d18e11c560a5424b7dbb2ccf4863b8
Joseda8 <jd.montoya.s8@gmail.com>
2019-05-22 17:55:25 -0600

    Se añadieron algunos INSERT

--- POPULATION.sql
+++ POPULATION.sql
@@ -8,3 +8,9 @@ INSERT INTO ROLE (role) VALUES('Control tower');
 INSERT INTO ROLE (role) VALUES('Flight Attendant');
 INSERT INTO ROLE (role) VALUES('Packager');
 INSERT INTO ROLE (role) VALUES('Customer');
+
+INSERT INTO USER (full_name, phone_number, email, username, password) 
+VALUES('José', '88888888', 'jd.m@gmail.com', 'Dookie', 'contraseña');
+
+INSERT INTO USER_ROLE (role_id, user_id) VALUES (1, 1);
+INSERT INTO USER_ROLE (role_id, user_id) VALUES (2, 1);
