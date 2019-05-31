7a695d23622dd7b6a91a67000b53f4b3d1d88599
Joseda8 <jd.montoya.s8@gmail.com>
2019-05-30 20:30:53 -0600

    USER a [USER]

--- POPULATION.sql
+++ POPULATION.sql
@@ -3,64 +3,64 @@ INSERT INTO ROLE (role) VALUES('Administrative ');
 INSERT INTO ROLE (role) VALUES('Scanner');
 INSERT INTO ROLE (role) VALUES('Receptionist');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('José', '88888888', 'jd.m@gmail.com', 'Dookie', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Aleeza Esparza', '25383036', 'alee.e@gmail.com', 'Aleezex', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Shania Goulding', '99352254', 'shan.g@gmail.com', 'XxShaniaxX', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Evelyn Farmer', '50365456', 'eve.f@gmail.com', 'Evelyn_f', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Anthony Knott', '97942296', 'thony.k@gmail.com', 'ElThony', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Sacha Christian', '42146801', 'sacha.ch@gmail.com', 'NotGrey', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Fern Chan', '35615597', 'fern.c@gmail.com', 'XiJinping', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Emmett Brookes', '40137307', 'emmett.b@gmail.com', 'Emmmm', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Willow Molloy', '92724572', 'will.m@gmail.com', 'WillyWonka', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Arjan Holland', '40525677', 'arjan.h@gmail.com', 'BayernFan', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Jacques Walmsley', '29094285', 'jacq.w@gmail.com', 'OhLaLa', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Ashley Pierce', '29865229', 'ash.p@gmail.com', 'NotKetchum', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Kiaan Talbot', '03916869', 'kiaan.t@gmail.com', 'Lmao', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Bruce Mahoney', '93543571', 'bruce.m@gmail.com', 'NotBanner', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Tomasz Jarvis', '06521679', 'tom.j@gmail.com', 'Tomasss', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Emilija Levine', '23438569', 'emilija.lev@gmail.com', 'AdamDaughter', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Aliya Donovan', '88324328', 'aliya.d@gmail.com', 'LandonDaughter', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Vinnie Carrillo', '48738008', 'vin.car@gmail.com', 'ViniiePiecillo', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Montell Winters', '16359057', 'mont.w@gmail.com', 'Pls_Rain_Not', 'xxx');
 
-INSERT INTO USER (full_name, phone_number, email, username, password) 
+INSERT INTO [USER] (full_name, phone_number, email, username, password) 
 VALUES ('Eve Butt', '35620583', 'eve.but@gmail.com', 'Baia', 'xxx');
 
 INSERT INTO USER_ROLE (role_id, user_id) VALUES (1, 1);
